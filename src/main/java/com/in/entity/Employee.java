3c19648a6008860574f46d59a039191e85630125
Susheel <141615684+SusheelKumarMalepati@users.noreply.github.com>
2024-12-23 23:23:59 +0530

    Update Employee.java

--- src/main/java/com/in/entity/Employee.java
+++ src/main/java/com/in/entity/Employee.java
@@ -4,10 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
-import lombok.Data;
 
 @Entity
-@Data
 public class Employee {
 	@Id
 	private Long employeeId;
@@ -16,4 +14,48 @@ public class Employee {
 	private Long employeePhoneNumber;
 	@Enumerated(EnumType.STRING)
 	private JobType jobType;
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmployeeEmail() {
+		return employeeEmail;
+	}
+	public void setEmployeeEmail(String employeeEmail) {
+		this.employeeEmail = employeeEmail;
+	}
+	public Long getEmployeePhoneNumber() {
+		return employeePhoneNumber;
+	}
+	public void setEmployeePhoneNumber(Long employeePhoneNumber) {
+		this.employeePhoneNumber = employeePhoneNumber;
+	}
+	public JobType getJobType() {
+		return jobType;
+	}
+	public void setJobType(JobType jobType) {
+		this.jobType = jobType;
+	}
+	public Employee(Long employeeId, String employeeName, String employeeEmail, Long employeePhoneNumber,
+			JobType jobType) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeEmail = employeeEmail;
+		this.employeePhoneNumber = employeePhoneNumber;
+		this.jobType = jobType;
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeEmail="
+				+ employeeEmail + ", employeePhoneNumber=" + employeePhoneNumber + ", jobType=" + jobType + "]";
+	}
 }
